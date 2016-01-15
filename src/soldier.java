262002a0183ce4273c25f0c244c6790771c0b370
denyskoech <dennis96ist@gmail.com>
2016-01-15 08:14:18 +0300

    Added main method and getters and setters

--- src/soldier.java
+++ src/soldier.java
@@ -2,4 +2,21 @@
 public class soldier {
 	private int Hitpoints;
 	private int Lives;
+	
+	//Getter
+	public int getHitpoints() {
+	    return Hitpoints;
+}
+	 // Setter
+	  public void setHitpoints(int c) {
+	    this.Hitpoints = c;
+	  }
+	//Getter
+		public int getLives() {
+		    return Lives;
+	}
+	// Setter
+		public void setHitpoints(int c) {
+		    this.Lives = c;
+		  }
 }
